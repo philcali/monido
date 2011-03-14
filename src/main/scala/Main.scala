a1e7d248ce6e8c0026433cf69018c807c975249b
Philip Cali <philip.cali@gmail.com>
2011-03-13 23:08:10 -0500

    Made Monido execute arbitrary cmd-line args

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ package com.github.philcali
 package monido
 
 import java.io.File
+import java.io.{BufferedReader, InputStreamReader}
 
 /**
  * Monido comes with a command line program that spits out changes in a shell
@@ -9,17 +10,34 @@ import java.io.File
  */
 object Main {
   def printHelp {
-    println("monido [-rh] [file|dir]")
+    println("monido [-rh] [file|dir] [-e=<command>]")
     def padleft(s: String) = "  %s" format(s)
     val text = List("-h         prints this help",
                     "-r         spawns recursive file monitor",
-                    "file|dir   monitors this file or directory (default .)")
+                    "file|dir   monitors this file or directory (default .)",
+                    "-e         executes command with changed file (defaults prints out to terminal)")
     text.map(padleft).foreach(println)
   }
+
+  val Exec = """-e=(.+)""".r
 
   def doIt(args: Array[String]) {
     val help = args.contains("-h")
     val recursive = args.contains("-r")
+    val command = args.find(_.startsWith("-e")) match {
+      case Some(Exec(cmd)) => 
+        (file: File) => {
+          val rt = Runtime.getRuntime()
+          val pr = rt.exec("%s %s" format(cmd, file.getAbsolutePath))
+          val in = new BufferedReader(new InputStreamReader(pr.getInputStream))
+          def read(in: BufferedReader): Unit = in.readLine match {
+            case line: String => println(line); read(in)
+            case _ => 
+          }
+          read(in)
+        }
+      case None => (file: File) => println(file)
+    }
     val dir = args.find(!_.startsWith("-")) match {
       case Some(a) => 
         val file = new File(a)
@@ -27,10 +45,11 @@ object Main {
       case None => new File(".")
     }
 
+
     if(help) { 
       printHelp
     } else { 
-      val monitor = FileMonido(dir.getAbsolutePath, recurse=recursive)(println)
+      val monitor = FileMonido(dir.getAbsolutePath, recurse=recursive)(command)
       println("Press Enter to quit")
       Console.readLine
       monitor.kill
