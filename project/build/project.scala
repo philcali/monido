f530e43629925b9b5d01d515420d25aaf5972c2c
Philip Cali <philip.cali@gmail.com>
2011-06-17 20:41:06 -0500

    Removing old stuff.

--- project/build/project.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-class Project(info: ProjectInfo) extends DefaultProject(info) with assembly.AssemblyBuilder {
-  val scalatest = "org.scalatest" % "scalatest" % "1.2"
-  val launcher = "org.scala-tools.sbt" % "launcher-interface" % "0.7.4" % "provided"
-
-  override def assemblyJarName = "monido.jar"
-}
